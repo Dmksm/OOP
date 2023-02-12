e27d62ed19b98c3ce2ebc419541a07d0dcf52b56
maksim-chernydev <maksim-chernydev@DESKTOP-U2UTPU1>
2023-02-12 12:53:44 +0300

    Добавил task1 и task2

---
+++ lab1/task1/replace.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <regex>
+
+void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
+	const std::string& searchString, const std::string& replacementString)
+{
+	std::string line;
+	const int minSearchStringLength = 1;
+	while (getline(input, line))
+	{
+		if (searchString.length() >= minSearchStringLength)
+		{
+			line = std::regex_replace(line, std::regex(searchString), replacementString);
+		}
+		output << line << std::endl;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	const int parametersNumber = 5;
+	if (argc != parametersNumber)
+	{
+		std::cout << "The number of parameters must be " << parametersNumber << " !\n";
+		return 1;
+	}
+	const std::string inputFileName = argv[1];
+	const std::string outputFileName = argv[2];
+	const std::string searchString = argv[3];
+	const std::string replacementString = argv[4];
+	std::ifstream inputFile(inputFileName);
+	std::ofstream outputFile(outputFileName);
+	if (!inputFile.is_open() || !outputFile.is_open())
+	{
+		std::cout << "Unable to open " << inputFileName << " or " << outputFileName <<
+			" file!\n";
+		return 1;
+	}
+	CopyStreamWithReplacement(inputFile, outputFile, searchString, replacementString);
+	outputFile.flush();
+	return 0;
+}
---
+++ lab1/task2/Radix.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+bool checkInAvailableRange(const int& numberSystem, const int& min,
+	const int& max)
+{
+	return ((min <= numberSystem) && (numberSystem <= max));
+}
+
+int main(int argc, char* argv[])
+{
+	const int parametersNumber = 4;
+	if (argc != parametersNumber)
+	{
+		std::cout << "The number of parameters must be " << parametersNumber << " !\n";
+		return 1;
+	}
+	const int sourceNotation = atoi(argv[1]);
+	const int destinationNotation = atoi(argv[2]);
+	const int value = atoi(argv[3]);
+	const int minNumberSystem = 2;
+	const int maxNumberSystem = 36;
+	if (!checkInAvailableRange(sourceNotation, minNumberSystem, maxNumberSystem) ||
+		!checkInAvailableRange(destinationNotation, minNumberSystem, maxNumberSystem))
+	{
+		std::cout << "Error! " << sourceNotation << " and " << destinationNotation <<
+			" must be between " << minNumberSystem << " and " << maxNumberSystem << " !\n";
+		std::cout << "Error! " << minNumberSystem <= sourceNotation << " and " << minNumberSystem <= destinationNotation << " must be between " << minNumberSystem << " and " << maxNumberSystem << " !\n";
+		return 1;
+	}
+	return 0;
+}
