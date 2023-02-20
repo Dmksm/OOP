7cb583c43a9d7ec36d4cfab806f8a3aaa3bd4c32
maksim-chernydev <maksim-chernydev@DESKTOP-U2UTPU1>
2023-02-20 12:09:07 +0300

    убрал replace,  сделал через append, протестировал на большом файле

--- lab1/task1/replace.cpp
+++ lab1/task1/replace.cpp
@@ -2,34 +2,49 @@
 #include <string>
 #include <fstream>
 
-void ReplaceSubstring(std::string &line, const std::string &searchString,
+std::string ReplaceSubstring(const std::string &line, const std::string &searchString,
 	const std::string &replacementString)
 {
 	const int minSearchStringLength = 1;
 	const size_t searchStringLength = searchString.length();
+	std::string resultString = line;
 	if (searchStringLength >= minSearchStringLength)
 	{
-		size_t pos = line.find(searchString);
 		const size_t notFound = std::string::npos;
-		while (pos != notFound)
+		const size_t startPos = 0;
+		const std::string emptyValue = "";
+		resultString = emptyValue;
+		size_t pos = startPos;
+		
+		while (pos < line.length())
 		{
-			line.replace(pos, searchStringLength, replacementString);
-			pos = line.find(searchString);
+			size_t searchStringPos = line.find(searchString, pos);
+			resultString.append(line, pos, searchStringPos - pos);
+			
+
+			if (searchStringPos == std::string::npos)
+			{
+				break;
+			}
+
+			resultString.append(replacementString);	
+			pos = searchStringPos + searchStringLength;
 		}
+		
 	}
+	return resultString;
 }
 
-void CopyStreamWithReplacement(std::ifstream& input, std::ostream& output,
+void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
 	const std::string& searchString, const std::string& replacementString)
 {
 	std::string line;
 	while (getline(input, line))
 	{
-		ReplaceSubstring(line, searchString, replacementString);
-		output << line << std::endl;
+		output << ReplaceSubstring(line, searchString, replacementString) << std::endl;
 	}
 }
-
+//проверить на милионе символов за сек
 bool CheckInputParametersNumber(int paramNumber)
 {
 	const int parametersNumber = 5;
@@ -50,7 +65,7 @@ bool CheckOpenFile(std::ifstream &file, const std::string &fileName)
 	}
 	return true;
 }
-
+//параметры закинуть в опциональную структуру на гит хабе есть , и вернуть или ошибку или готовые файлы
 int main(int argc, char* argv[])
 {
 	if (!CheckInputParametersNumber(argc))
