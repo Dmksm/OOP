94a9b4757ba83a6105602255ca040af19d78c832
maksim-chernydev <maksim-chernydev@DESKTOP-U2UTPU1>
2023-03-19 22:32:43 +0300

    сделал цикл для работы с файлами

--- lab2/HTMLDecode/HTMLDecode/main.cpp
+++ lab2/HTMLDecode/HTMLDecode/main.cpp
@@ -3,7 +3,9 @@
 int main()
 {
     std::string html;
-    getline(std::cin, html);
-    std::cout << HtmlDecode(html) << std::endl;
+	while (getline(std::cin, html))
+	{
+		std::cout << HtmlDecode(html) << std::endl;
+	}
     return 0;
 }
